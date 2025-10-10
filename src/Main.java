7fb8d83942f61c0a0dae3850d615264b81734e4f
Anshul <anshul2006n@gmail.com>
2025-10-10 21:57:06 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        try { //To Support Sleeping Threads... millis:30000(3Sec)
+            
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
