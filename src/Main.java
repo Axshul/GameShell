55ebcba67abe1bcb10bc6ae0608539e317a63343
Axshul <anshul2006n@gmail.com>
2025-10-27 10:51:14 +0530

    Testing GiTea PUSH services

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        try { //To Support Sleeping Threads... millis:30000(3Sec)
-            
+        try { //To Support Sleeping Threads... millis:3000(3Sec)
+            System.out.println("Sleeping Thread T1");
+            Thread.sleep(2000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
         }
