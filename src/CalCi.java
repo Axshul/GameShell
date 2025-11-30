ccaa246c3a9f9e25ea2dd1e80c7c7f301431e4a4
Anshul <anshul2006n@gmail.com>
2025-11-30 19:32:20 +0530

    Update CalCi.java

--- src/CalCi.java
+++ src/CalCi.java
@@ -1,8 +1,41 @@
+import java.util.Scanner;
+import java.util.LinkedList;
+
 public class CalCi {
+
+    public static void LogicalState(String UserEquation) {
+        double Solution = 0.0;
+
+
+
+        System.out.println(UserEquation + " == "+Solution);
+    }
     public static void main(String[] args) {
         // Variable's Land
+        Scanner Scan = new Scanner(System.in);
+        boolean CalCiActive = true;
+        String[] LastOperations = new String[9];
+
+
 
         System.out.println("CalCI For The Next Generation you!");
-        
+
+        while (CalCiActive) {
+            System.out.print("> ");
+            String UserEquation = Scan.nextLine();
+
+
+            // LOGICAL STATEMENTS
+            LogicalState(UserEquation);
+
+
+
+            // EXIT STATEMENT CODE:0
+            if(UserEquation.equals("/exit")) {
+                System.out.println("BYE BYE!");
+                CalCiActive = false;
+            }
+        }
+
     }
 }
